9aa281e655a89000a4518a4d3081a128f95b5c90
Mohammed Hossain <jamalhossain1629@gmail.com>
2021-02-24 08:58:28 -0500

    done

--- LoggingKata/Program.cs
+++ LoggingKata/Program.cs
@@ -31,17 +31,51 @@ namespace LoggingKata
 
             // DON'T FORGET TO LOG YOUR STEPS
 
+
             // Now that your Parse method is completed, START BELOW ----------
 
             // TODO: Create two `ITrackable` variables with initial values of `null`. These will be used to store your two taco bells that are the farthest from each other.
             // Create a `double` variable to store the distance
+            ITrackable tacoBell1 = null;
+            ITrackable tacoBell2 = null;
+
+            double distance = 0;
+
 
             // Include the Geolocation toolbox, so you can compare locations: `using GeoCoordinatePortable;`
 
             //HINT NESTED LOOPS SECTION---------------------
             // Do a loop for your locations to grab each location as the origin (perhaps: `locA`)
-
             // Create a new corA Coordinate with your locA's lat and long
+            for (int i=0; i <locations.Length; i++)
+            {
+                var locA = locations[i];
+               
+                var corA = new GeoCoordinate();
+                
+                corA.Latitude = locA.Location.Latitude;
+                corA.Longitude = locA.Location.Longitude;
+
+                for (int j=0; j < locations.Length; j++)
+                {
+                    var locB = locations[j];
+                    var corB = new GeoCoordinate(locB.Location.Latitude, locB.Location.Longitude);
+
+                    if (corA.GetDistanceTo(corB)> distance)
+                    {
+                        distance = corA.GetDistanceTo(corB);
+                        tacoBell1 = locA;
+                        tacoBell2 = locB;
+                    }
+                }
+
+                Console.WriteLine($"The furthest TacoBells are {tacoBell1.Name} and {tacoBell2.Name}");
+                Console.WriteLine();
+                Console.WriteLine($"they are {distance} meters from each other.");
+
+
+            }
+            
 
             // Now, do another loop on the locations with the scope of your first loop, so you can grab the "destination" location (perhaps: `locB`)
 
---
+++ LoggingKata/TacoBell.cs
@@ -0,0 +1,12 @@
+﻿namespace LoggingKata
+{
+    internal class TacoBell
+    {
+        public TacoBell()
+        {
+        }
+
+        public string Name { get; internal set; }
+        public object Location { get; internal set; }
+    }
+}
